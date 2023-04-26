dc3cad47ade3064bfcdc34d7d2aa2686d3ecf315
juan sacco <juandsacco@gmail.com>
2023-04-26 19:06:29 -0300

    modifico texto y estilado final con css

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -6,24 +6,32 @@ const DarkMode = () => {
     const mode = useSelector((state) => state.mode)
 
     const dispatch = useDispatch();
-    
+
     return(
         <>
             {
                 mode === true ? 
-                <div className="lightMode">
-                    <button onClick={() => dispatch(lightMode())}>LightMode</button>
-                    <div className="lightMode">
-                        <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
-                        <p>Si usted ingresó en la página para obtener informacion o comprar algo, se encuentra en el lugar equivocado.</p>
+                <div className="modeContainer">
+                    <div className="lightModeContainer">
+                        <div className="divButton">
+                        <button className="btnDark" onClick={() => dispatch(lightMode())}>Dark</button>
+                        </div>
+                        <div className="lightMode">
+                            <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
+                            <p>Cree ésta página para afianzar conceptos de redux, iniciando la app con vite.</p>
+                        </div>
                     </div>
                 </div>
                 :
-                <div className="darkMode">
-                    <button onClick={() => dispatch(darkMode())}>DarkMode</button>
-                    <div className="darkMode">
-                        <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
-                        <p>Si usted ingresó en la página para obtener informacion o comprar algo, se encuentra en el lugar equivocado.</p>
+                <div className="modeContainer">
+                    <div className="darkModeContainer">
+                        <div className="divButton">
+                        <button onClick={() => dispatch(darkMode())}>Standar</button>
+                        </div>
+                        <div className="darkMode">
+                            <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
+                            <p>Cree ésta página para afianzar conceptos de redux, iniciando la app con vite.</p>
+                        </div>
                     </div>
                 </div>
             }
