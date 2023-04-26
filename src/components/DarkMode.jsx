e718dad9a5d64df48a70b6632e49e1f75600386b
juan sacco <juandsacco@gmail.com>
2023-04-26 16:00:29 -0300

    agrego ternaria para cambiar los modos y estilos

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -6,19 +6,29 @@ const DarkMode = () => {
     const mode = useSelector((state) => state.mode)
 
     const dispatch = useDispatch();
-
+    console.log(mode);
     return(
         <>
-            <div className="contenedor"> 
-                {mode}
-                <button onClick={() => dispatch(darkMode())}> Dark Mode </button>
-                <button onClick={() => dispatch(lightMode())}> Light Mode </button>
-            <div>
-                <h1>Bienvenidos a la página donde solo podran cambiar el modo</h1>
-                <p>Si usted ingresó en la página para obtener informacion o comprar algo, se encuentra en el lugar equivocado.</p>
-            </div>
-            </div>
+            {
+                mode === true ? 
+                <div className="lightMode">
+                    <button onClick={() => dispatch(lightMode())}>LightMode</button>
+                    <div className="lightMode">
+                        <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
+                        <p>Si usted ingresó en la página para obtener informacion o comprar algo, se encuentra en el lugar equivocado.</p>
+                    </div>
+                </div>
+                :
+                <div className="darkMode">
+                    <button onClick={() => dispatch(darkMode())}>DarkMode</button>
+                    <div className="darkMode">
+                        <h4>Bienvenidos a la página donde solo podran cambiar el modo</h4>
+                        <p>Si usted ingresó en la página para obtener informacion o comprar algo, se encuentra en el lugar equivocado.</p>
+                    </div>
+                </div>
+            }
         </>
+        
     )
 }
 
